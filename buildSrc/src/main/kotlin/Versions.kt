7dd0f816902276cd624cdda204baf32a042fbff8
Élise Rubio <elise.rubio@4sh.fr>
2021-05-23 13:02:08 +0200

    :tada: Initialize gradle project

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,5 @@
+object Versions {
+    const val junit = "5.7.0"
+    const val strikt = "0.21.0"
+    const val kotlin = "1.4.10"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "google-code-jam-2021-solutions"
+
+include(
+    "solutions:smaller-strings"
+)
