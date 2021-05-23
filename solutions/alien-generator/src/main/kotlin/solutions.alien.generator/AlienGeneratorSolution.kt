e7259341b75b9213571f4c3138b614c23a2ca99f
Élise Rubio <elise.rubio@4sh.fr>
2021-05-23 14:28:59 +0200

    :sparkles: Use polynoms to reduce time

--- solutions/alien-generator/src/main/kotlin/solutions.alien.generator/AlienGeneratorSolution.kt
+++ solutions/alien-generator/src/main/kotlin/solutions.alien.generator/AlienGeneratorSolution.kt
@@ -1,4 +1,10 @@
+import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 import java.math.BigInteger
+import java.math.MathContext
+import java.math.RoundingMode
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 fun readLineToInt() = readLine()!!.toInt()
 
@@ -10,22 +16,28 @@ fun main(args: Array<String>) {
     }
 }
 
+fun isValidK(K: BigInteger, G: BigInteger): Boolean {
+    val delta = BigDecimal.valueOf(0.25) +
+            BigDecimal.valueOf(2) * (((K * (K - BigInteger.ONE)).toBigDecimal() / BigDecimal.valueOf(2)) + G.toBigDecimal())
+
+    if(delta <= ZERO) {
+        return false
+    }
+
+    val root = BigDecimal.valueOf((-1.0/2.0)) + delta.sqrt(MathContext(4, RoundingMode.HALF_UP))
+
+    return root.toBigInteger() != K && root.stripTrailingZeros().scale() <= 0
+}
+
 fun countPossibleValuesForK(goldBarsObjective: Int): Int {
     var i = BigInteger.ONE
     var numberOfPossibleValuesForK = 0
     val goldBarsObjectiveBigInteger =  BigInteger.valueOf(goldBarsObjective.toLong())
-    while (i <= goldBarsObjectiveBigInteger) {
-        var j = i
-        var sum = BigInteger.ZERO
-        while(sum < goldBarsObjectiveBigInteger) {
-            sum+=j
-            ++j
-        }
-
-        if(sum == goldBarsObjectiveBigInteger) {
+    while (i < goldBarsObjectiveBigInteger) {
+        if(isValidK(i, goldBarsObjectiveBigInteger)) {
             ++numberOfPossibleValuesForK
         }
         ++i
     }
-    return numberOfPossibleValuesForK
+    return numberOfPossibleValuesForK+1
 }
